0aba584be74cc468c83c0fbc6519d747c137ff74
Marlon Menendez <marlon.menendezg@gmail.com>
2024-04-11 10:40:25 -0600

    update run file

--- run.py
+++ run.py
@@ -1,18 +1,1 @@
-import click
-
-from src.tools.hyperparameters_tuning import get_best_params
-from src.tools.train_model import train_model
-from src.tools.predict import single_prediction
-
-
-@click.group()
-def cli():
-    pass
-
-
-cli.add_command(get_best_params)
-cli.add_command(train_model)
-cli.add_command(single_prediction)
-
-if __name__ == "__main__":
-    cli()
+from tensorflow import
